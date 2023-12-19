fc301040d0a50f1ed4d9570e4c16a17bf40dab73
Nicolas Roche <hadarak@nicolass-mbp.home>
2023-12-19 12:51:16 +0100

    Init Website

---
+++ app/pages/area/_id.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>{{ areaName }} Boulders</h1>
+
+        <!-- Display the name of the area -->
+        <h2>{{ areaName }}</h2>
+
+        <!-- Display two boulders for the area -->
+        <div v-for="boulder in boulders" :key="boulder.id">
+            <p>{{ boulder.name }}</p>
+        </div>
+    </div>
+</template>
+  
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    data() {
+        return {
+            areaName: '',
+            boulders: [
+                { id: 'boulder1', name: 'Boulder 1' },
+                { id: 'boulder2', name: 'Boulder 2' },
+                // Add more boulders as needed
+            ],
+        };
+    },
+    async asyncData({ params }) {
+        // Fetch data based on the area ID (params.id)
+        // For simplicity, you can set areaName directly, or fetch data from an API
+        return { areaName: `Area ${params.id}` };
+    },
+});
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,41 @@
+
+<template>
+    <div>
+        <h1>{{ pageTitle }}</h1>
+        <header>
+            <h2>{{ headerText }}</h2>
+        </header>
+
+        <ul>
+            <li v-for="area in areas" :key="area.id">
+                <router-link :to="'/area/' + area.id">{{ area.name }}</router-link>
+            </li>
+        </ul>
+
+        <!-- Add more content or components here as needed -->
+    </div>
+</template>
+  
+<script>
+import { defineComponent } from 'vue';
+import '~/assets/css/main.css'
+
+export default defineComponent({
+    data() {
+        return {
+            pageTitle: 'Bouldering in South of France',
+            headerText: 'Discover thousands of problems & circuits for all levels.',
+            areas: [
+                { id: 'naviteau', name: 'Naviteau' },
+                { id: 'leprieure', name: 'Le Prieuré' },
+                // Add more areas as needed
+            ],
+        };
+    },
+});
+</script>
+  
+<style scoped>
+/* Add your styles here */
+</style>
+  
